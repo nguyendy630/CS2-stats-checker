3408a4416a46e4ffd99a8afab2b333fa6b8d9bbf
Dylan Nguyen <112450882+nguyendy630@users.noreply.github.com>
2024-09-08 22:54:34 +0000

    Issued Steam Web Request to API for User Stats

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,40 @@
 import './App.css'
-import {useState, useEffect} from 'react'
+import { useState, useEffect } from 'react'
 import axios from 'axios'
 
 
 function App() {
-  const[results, setResults] = useState([]);
+  const [results, setResults] = useState([]);
   const controller = new AbortController();
-  
+
   useEffect(() => {
     async function fetchData() {
-      const response = axios.get("http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/", {
+      axios.get("http://api.steampowered.com/ISteamUserStats/GetUserStatsForGame/v0002/", {
         params: {
           key: import.meta.env.VITE_STEAM_WEB_API_KEY,
-          steamids: "76561198274029909"
+          steamids: "76561198274029909",
+          appid: 730
         }
+      }).then(
+        response => {
+          console.log(response.data)
+        }
+      ).catch(err => {
+        console.log(err)
       })
-      const data = await response.json();
-      setResults(data);
     }
 
     fetchData();
 
     console.log(results, "Results");
+    console.log('API Key:', import.meta.env.VITE_STEAM_WEB_API_KEY);
   }, [])
 
   return (
     <>
       <div className='container'>
         <h1 className="text-xl">Fetching Data</h1>
-      
+
       </div>
     </>
   )
