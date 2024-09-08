1ded780251f023afccecdc3d7fc3458631f57178
Dylan Nguyen <nguyendylan2468@gmail.com>
2024-09-08 18:14:46 -0400

    Added get request with axios to Steam Web API

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,35 @@
 import './App.css'
+import {useState, useEffect} from 'react'
+import axios from 'axios'
+
 
 function App() {
+  const[results, setResults] = useState([]);
+  const controller = new AbortController();
+  
+  useEffect(() => {
+    async function fetchData() {
+      const response = axios.get("http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/", {
+        params: {
+          key: import.meta.env.VITE_STEAM_WEB_API_KEY,
+          steamids: "76561198274029909"
+        }
+      })
+      const data = await response.json();
+      setResults(data);
+    }
+
+    fetchData();
+
+    console.log(results, "Results");
+  }, [])
 
   return (
     <>
+      <div className='container'>
+        <h1 className="text-xl">Fetching Data</h1>
       
+      </div>
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,14 @@
-import { StrictMode } from 'react'
+import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import {NextUIProvider} from '@nextui-org/react'
+import './index.css'
+
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <React.StrictMode>
+  <NextUIProvider>
     <App />
-  </StrictMode>,
+  </NextUIProvider>
+</React.StrictMode>,
 )
