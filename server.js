6cda05b833e399d27191254858e8ec36d828a6ce
Dylan Nguyen <112450882+nguyendy630@users.noreply.github.com>
2024-09-18 03:35:55 +0000

    added server.js that makes request to Steam Web API:
    BUG (browser compatibility)
    
    Added Packages express, cors, axios, dotenv
    
    Removed UserStats.jsx for server.js

---
+++ server.js
@@ -0,0 +1,31 @@
+import express, { response } from "express"
+import cors from "cors"
+import axios from "axios"
+import dotenv from "dotenv"
+
+dotenv.config()
+
+const app = express()
+const PORT = process.env.PORT || 8000;
+
+const options = {
+    key: process.env.VITE_STEAM_WEB_API_KEY,
+    steamid: "76561198274029909",
+    appid: 730
+}
+
+app.get('/results', (req, res) => {
+    axios.request(`https://api.steampowered.com/ISteamUserStats/GetUserStatsForGame/v0002/?appid=${options.appid}&key=${options.key}&steamid=${options.steamid}`)
+        .then((response) => {
+            res.json(response.data)
+        }).catch((error) => {
+            console.log(error)
+        })
+})
+
+
+app.get('/', (req, res) => {
+    res.json("Hi")
+})
+
+app.listen(8000, () => console.log(`Sever is running on Port ${PORT}`)) 
--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,36 @@
 import './App.css'
 import { useState, useEffect } from 'react'
-import UserStats from './components/UserStats'
-
+import axios from "axios";
+import { response } from 'express';
 
 function App() {
+  const [userData, setUserData] = useState(null);
+
+  useEffect(() => {
+
+    async function fetchData() {
+      const options = {
+        method: 'GET',
+        url: 'http://localhost:8000/results',
+      }
+  
+      axios.request(options).then((response) => {
+        console.log(response.data);
+        setUserData(response.data);
+      }).catch((error) => {
+        console.log(error)
+      })
+    }
+    
+    fetchData()
+  },[])
+
   return (
     <>
       <div className='container'>
         <h1 className="text-xl">Fetching Data</h1>
         <ul>
-          <UserStats/>
+          <li></li>
         </ul>
       </div>
     </>
