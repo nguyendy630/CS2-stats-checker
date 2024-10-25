dd424b2dd7a79cffcee3c16c74cf4dbd15b9297b
Dylan Nguyen <112450882+nguyendy630@users.noreply.github.com>
2024-10-25 04:32:28 +0000

    Added validateSteamID function, only runs once must run constant

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,18 @@ import FetchData from "./components/FetchData"
 import { Input } from "@nextui-org/input";
 
 function App() {
-  const steamID = "76561198274029909";
-  let newSteamId = "76561198961634260";
+  const [currentSteamID, setSteamID] = useState("");
+
+  // const steamID = "76561198274029909";
+  // let newSteamId = "76561198961634260";
+
   
   return (
     <>
-      <FetchData steamid={newSteamId}/>
+        <div className="App">
+          <Input value={currentSteamID} onValueChange={setSteamID}/>
+          <FetchData steamid={currentSteamID}/>
+        </div>
     </>
   )
 }
